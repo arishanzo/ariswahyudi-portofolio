bf4c39de238377a3a3b5adc3ab5a304c7a001f48
arishanzo <arishanzo86@gmail.com>
2026-08-05 22:04:38 +0700

    refactor: remove unused Contact component import from App.vue

--- ariswahyudi-portofolio/src/App.vue
+++ ariswahyudi-portofolio/src/App.vue
@@ -6,7 +6,6 @@ import About from './components/About.vue'
 import Skills from './components/Skills.vue'
 import Experience from './components/Experience.vue'
 import Projects from './components/Projects.vue'
-import Contact from './components/Contact.vue'
 import Footer from './components/Footer.vue'
 
 onMounted(() => {
